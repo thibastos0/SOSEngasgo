00cd0456ff5cc523f93044cb09572e2932569e3c
thibastos0 <thibastos@live.com>
2025-10-30 10:23:09 -0300

    commit inicial para o PI SOS Engasgo no framework spring boot.

---
+++ src/main/java/com/example/SOSEngasgo/SosEngasgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SOSEngasgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosEngasgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosEngasgoApplication.class, args);
+	}
+
+}
