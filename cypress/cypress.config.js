78947d5335c21de1536fb578ad13f26552b66171
thibastos0 <thibastos@live.com>
2025-11-30 20:21:32 -0300

    supportFile false

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
-    video: false
+    video: false,
+    supportFile: false
   }
 });
